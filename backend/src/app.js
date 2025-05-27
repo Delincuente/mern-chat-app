8543f0474aec93bf758341b748317e0c7ef3097b
CRIMINAL <butani.hardik53@gmail.com>
2025-05-27 17:19:52 +0530

    rouet apdate

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,7 @@ dotenv.config();
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "path";
 
 import connectDB from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -13,8 +12,7 @@ import messageRoutes from "./routes/message.route.js";
 import seedsRoutes from "./routes/seeds.route.js";
 import { app, server } from "./lib/socket.io.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 connectDB();
@@ -36,7 +34,7 @@ app.use("/api/message", messageRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
